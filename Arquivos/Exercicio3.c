8bb0853af58646516e3621e9811075b05c8e31d5
Pedro Antonio Ciciliano Mendes <pedrocmendescoc2@gmail.com>
2022-07-27 14:17:44 -0300

    Fiz o exercício 3 de arquivos

---
+++ Arquivos/Exercicio3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    char nomes[20];
+
+    FILE *arq;
+
+    arq = fopen("Nomes.txt","w");
+
+    if ( arq )
+    {
+        do 
+        {
+            printf("\n - Digite um nome ou 0 para sair: ");
+            fgets(nomes,20,stdin);
+            fputs(nomes,arq);
+        }
+        while ( nomes[0] != '0');
+    }
+
+    else
+    {
+        printf("\n - Nao foi possivel criar o arquivo!");
+    }
+    
+    fclose(arq);
+
+    printf("\n\n\n");
+    system("pause");
+
+    return 0;
+}
